349b4d5145b320ac91e581ee71dd5aac0c494761
Joel Silva <henriquesa@ipvc.pt>
2021-02-27 16:59:30 +0000

    option menu

--- app/src/main/java/com/example/a2_criterios_parte1_transversais_p2/Main2Activity.kt
+++ app/src/main/java/com/example/a2_criterios_parte1_transversais_p2/Main2Activity.kt
@@ -2,7 +2,11 @@ package com.example.a2_criterios_parte1_transversais_p2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
 
 class Main2Activity : AppCompatActivity() {
 
@@ -16,4 +20,33 @@ class Main2Activity : AppCompatActivity() {
             text = message
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu2,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.create_new -> {
+                Toast.makeText(this,"create_new", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao2 -> {
+                Toast.makeText(this,"opcao2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao3 -> {
+                Toast.makeText(this,"opcao3", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao4 -> {
+                Toast.makeText(this,"opcao4", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
