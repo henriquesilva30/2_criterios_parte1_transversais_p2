0ec03731cf6ff2f46774801f78f19ef2a0e9ec67
Joel Silva <henriquesa@ipvc.pt>
2021-02-27 13:42:24 +0000

    botoes e envio de texto

--- app/src/main/java/com/example/a2_criterios_parte1_transversais_p2/MainActivity.kt
+++ app/src/main/java/com/example/a2_criterios_parte1_transversais_p2/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.a2_criterios_parte1_transversais_p2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    fun submeter(view: View) {
+
+        var editHelloWorld = findViewById<EditText>(R.id.editTextnome)
+        Toast.makeText(this,editHelloWorld.text, Toast.LENGTH_SHORT).show()
+        findViewById<TextView>(R.id.textView1).setText(editHelloWorld.text)
 
     }
 }
