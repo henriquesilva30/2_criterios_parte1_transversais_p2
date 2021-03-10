c6322056e18c4f9667cbce406de81c4cfbbd378b
Joel Silva <henriquesa@ipvc.pt>
2021-03-10 15:42:36 +0000

    sharedpreference explode

--- app/src/main/java/com/example/a2_criterios_parte1_transversais_p2/MainActivity.kt
+++ app/src/main/java/com/example/a2_criterios_parte1_transversais_p2/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.a2_criterios_parte1_transversais_p2
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -18,7 +22,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sharedPref: SharedPreferences = getSharedPreferences(
+            getString(R.string.preference_file_key), Context.MODE_PRIVATE)
 
+        val soundValue = sharedPref.getBoolean(getString(R.string.bool),false)
+        Log.d("***SHAREDPREF","READ $soundValue")
+
+        if(soundValue){
+            findViewById<CheckBox>(R.id.checkbox).isChecked=true
+        }
+
+    }
+
+    fun checkboxClicked(view: View){
+        if (view is CheckBox ){
+            val sharedPref: SharedPreferences = getSharedPreferences(
+            getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            with(sharedPref.edit()){
+                putBoolean(getString(R.string.bool), view.isChecked)
+                commit()
+            }
+            Log.d("***SHAREDPREF","Changed to  ${view.isChecked}")
+        }
     }
 
     fun submeter(view: View) {
